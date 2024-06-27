418950065f015a58d64b2f67abf186737df2d67e
MagretBro <magretbro@gmail.com>
2024-06-27 16:04:45 +0700

    реализована логика изменения цвета в завис от значения в блоке биржи

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -17,4 +17,25 @@ export class MainComponent {
     };
   }
 
+
+  public data = [
+    { lastPrice: '40000', change: '+0.50%', lowHigh: '39000/41000', volume: '1000 BTC' },
+    { lastPrice: '35000', change: '-4.30%', lowHigh: '34000/36000', volume: '900 BTC' },
+    { lastPrice: '35000', change: '-1.16%', lowHigh: '34000/36000', volume: '900 BTC' },
+    { lastPrice: '35000', change: '+4.84%', lowHigh: '34000/36000', volume: '900 BTC' },
+
+ 
+    // Добавьте больше данных по необходимости
+  ];
+
+  getChangeClass(change: string): string {
+    if (change.startsWith('+')) {
+      return 'change-positive';
+    } else if (change.startsWith('-')) {
+      return 'change-negative';
+    } else {
+      return '';
+    }
+  }
+
 }
